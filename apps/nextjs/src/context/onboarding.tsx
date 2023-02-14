f7e6fd9de8e84d1d1cbb2419f9003e393fee3924
Zachary Lee <w.zachary.lee@gmail.com>
2023-02-14 15:43:11 -0500

    typefixes

--- apps/nextjs/src/context/onboarding.tsx
+++ apps/nextjs/src/context/onboarding.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { createContext, useContext, useEffect, useState } from "react";
 
 import { getLocalStorage, setLocalStorage } from "~/utils/helpers/localStorage";
